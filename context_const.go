00c0607caf8b7fbb805f1806565dd84b189b9599
RyougiNevermore <ryougi.nevermore@hotmail.com>
2019-11-24 05:37:21 +0800

    add response writer

---
+++ context_const.go
@@ -0,0 +1,28 @@
+package literoute
+
+const (
+	ContentTypeHeaderKey            = "Content-Type"
+	LastModifiedHeaderKey           = "Last-Modified"
+	IfModifiedSinceHeaderKey        = "If-Modified-Since"
+	CacheControlHeaderKey           = "Cache-Control"
+	ETagHeaderKey                   = "ETag"
+	ContentDispositionHeaderKey     = "Content-Disposition"
+	ContentLengthHeaderKey          = "Content-Length"
+	ContentEncodingHeaderKey        = "Content-Encoding"
+	GzipHeaderValue                 = "gzip"
+	AcceptEncodingHeaderKey         = "Accept-Encoding"
+	VaryHeaderKey                   = "Vary"
+	ContentBinaryHeaderValue        = "application/octet-stream"
+	ContentHTMLHeaderValue          = "text/html"
+	ContentJSONHeaderValue          = "application/json"
+	ContentJSONProblemHeaderValue   = "application/problem+json"
+	ContentXMLProblemHeaderValue    = "application/problem+xml"
+	ContentJavascriptHeaderValue    = "application/javascript"
+	ContentTextHeaderValue          = "text/plain"
+	ContentXMLHeaderValue           = "text/xml"
+	ContentXMLUnreadableHeaderValue = "application/xml"
+	ContentMarkdownHeaderValue      = "text/markdown"
+	ContentYAMLHeaderValue          = "application/x-yaml"
+	ContentFormHeaderValue          = "application/x-www-form-urlencoded"
+	ContentFormMultipartHeaderValue = "multipart/form-data"
+)
